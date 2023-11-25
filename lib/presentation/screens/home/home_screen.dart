74eeec32aa938c8df62bcc91fcc2662edf546119
coco1690 <darh90@gmail.com>
2023-11-25 05:00:50 -0500

    implementacion del context.push navigator

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
  class HomeScreen extends StatelessWidget {
@@ -49,7 +50,7 @@ class _CustomListTitle extends StatelessWidget {
       title:    Text(menuItem.title),
       subtitle: Text(menuItem.subTitlle),
       onTap: () {
-        // TODO: aca va  la navegacion
+        context.push(menuItem.link);
       },
     );
   }
