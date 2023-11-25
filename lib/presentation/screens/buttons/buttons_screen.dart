875d85d17cdf66d2a9788f77402c7b063aefff7d
coco1690 <darh90@gmail.com>
2023-11-25 16:34:13 -0500

    se instala el botonflotante

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   const ButtonsScreen({super.key});
@@ -6,8 +7,25 @@ class ButtonsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar( title: const Text('Botones'),),
-      body: const Placeholder(),
+
+      body:  _ButtonsView(),
+
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: (){
+          context.pop();
+        },     
+      )
     );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CardScreen extends StatelessWidget {
   const CardScreen({super.key});
@@ -6,8 +7,17 @@ class CardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       appBar: AppBar( title: const Text('Tarjetas o Cards'),),
+
       body: const Placeholder(),
+      
+       floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: (){
+          context.pop();
+        },     
+      )
     );
   }
 }
